e37932ab77f1566180898f0d2a31c235efee2737
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2022-06-26 00:12:07 -0500

    Contador terminado

--- FirstApp/src/app/app.component.ts
+++ FirstApp/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FirstApp';
+  public title: string = 'App Contador';
+  public acumulado: number = 0;
+  public base: number = 5;
+
+  acumular(numero: number) {
+    this.acumulado += numero;
+  }
 }
