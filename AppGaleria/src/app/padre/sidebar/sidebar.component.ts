373a5b3d23f05156b6aec3f0324d0d7200c90a80
“WRocano” <wrocano@baustro.fin.ec>
2022-06-28 09:29:41 -0500

    App galería, separación en componentes, uso de servicios, viewchild, entre otros

---
+++ AppGaleria/src/app/padre/sidebar/sidebar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ServiciosService } from '../servicios.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent   {
+
+
+   get historial (){
+   return this.servicios.arreglobusquedas;
+   }
+
+
+  constructor(
+
+    private servicios:ServiciosService
+  ) {
+
+
+   }
+
+
+
+}
