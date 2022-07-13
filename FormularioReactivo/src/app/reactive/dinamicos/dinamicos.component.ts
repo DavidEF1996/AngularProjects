31e5f796730021daecadaad75c882f2ab1f2c6f1
DavidEF1996 <59062590+DavidEF1996@users.noreply.github.com>
2022-07-13 00:14:35 -0500

    inicio de formularios con arrays, y listado de elementos

--- FormularioReactivo/src/app/reactive/dinamicos/dinamicos.component.ts
+++ FormularioReactivo/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', Validators.required],
+    favoritos: this.fb.array(['Metal', 'Resident'], Validators.required),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  validarCampos(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
   }
 
+  get formulario() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  //ARRAYS
+  //1. CREAR ARRAY
+  //2. en el html especificar la propiedad formarrayname
+  //3. recorrer los elementos del arreglo
+  //4. en el control name pondremos el indice
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
 }
