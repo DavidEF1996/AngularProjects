35f98167a162eb4a2fee6650927d1797492c46f7
“WRocano” <wrocano@baustro.fin.ec>
2022-07-14 13:06:42 -0500

    App Formularios reactivos primera parte finalizado

--- FormularioReactivo/src/app/reactive/dinamicos/dinamicos.component.ts
+++ FormularioReactivo/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -9,7 +9,7 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DinamicosComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required],
-    favoritos: this.fb.array(['Metal', 'Resident'], Validators.required),
+    favoritos: this.fb.array([]),
   });
 
   validarCampos(campo: string) {
@@ -23,11 +23,31 @@ export class DinamicosComponent implements OnInit {
     return this.miFormulario.get('favoritos') as FormArray;
   }
 
+
+  agregar(){
+
+   if(this.nuevoFavorito.invalid){return;}
+    this.formulario.push(this.fb.control(this.nuevoFavorito.value,Validators.required));
+    this.nuevoFavorito.reset();
+
+  }
+
+  eliminar(indice:number){
+  this.formulario.removeAt(indice);
+  }
+
+  nuevoFavorito: FormControl=this.fb.control('');
   //ARRAYS
   //1. CREAR ARRAY
   //2. en el html especificar la propiedad formarrayname
   //3. recorrer los elementos del arreglo
   //4. en el control name pondremos el indice
+  //5. para agregar mas a la lista, creamos un objeto independiente
+  //6. agregar el elemento al html pero como un elemento independiente,  [formcontrol]
+  //7. agregar al array
+  //8. elimianr del array
+  //9. el eliminar se puede usar el metodo que lista y removerle de allí
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
--- FormularioReactivo/src/app/reactive/switches/switches.component.ts
+++ FormularioReactivo/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario = this.fb.group({
+
+    genero:['M',Validators.required],
+    notificacion:[true,Validators.required],
+    terminosCondiciones:[true,Validators.requiredTrue]
+
+  })
+
+  persona={
+    genero:'F',
+    notificacion:false
+  }
+
+
+
+
+  constructor(private fb:FormBuilder) {
+
+    this.miFormulario.valueChanges.subscribe(e=>{
+      this.persona = e
+    })
+  }
 
   ngOnInit(): void {
+
+   // this.miFormulario.reset(this.persona)
+
+    //si es que quisieramos darle por defecto
+    this.miFormulario.reset({
+      ...this.persona, //extraemos todo de persona y
+      terminosCondiciones:true // agregamos la propiedad con el valor deseado
+    })
+
+
   }
 
 }
